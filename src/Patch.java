fc753836143cb73549f67bc2b49d61799b9f7761
zyud <zyud@seas.upenn.edu>
2018-01-29 10:49:52 -0500

    updated Patch class coord system from double[] to Vector3D

--- src/Patch.java
+++ src/Patch.java
@@ -1,19 +1,19 @@
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
 /**
  * The patch class. A patch is a small portion of a surface.
  * A patch is usually a rectangle but can be any 4-sided polygon.
  * @author zyud
  */
 public class Patch {
-	private double[][] coords; // 4x3 array of the x,y,z coords of the 4 corner pts
-	// the coords array stores adjacent corner points of the Patch in a 
-	// counter-clockwise manner. (e.g. top left, bottom left, bottom right, top right)
-	private double[] normal; // 3D unit vector pointing to normal of patch
+	private Vector3D[] coords; // a Vector3D array containing the coordinates
+	private Vector3D normal; // 3D unit vector pointing to normal of patch
 	private double reflectance; // reflectance of Patch, between 0 and 1
-	private double[] center; // center of Patch
+	private Vector3D center; // center of Patch
 	private double incident; // amount of light hitting Patch
 	private double excident; // amount of light leaving Patch
 
-	public Patch(double[][] coords, double[] normal, double reflectance) {
+	public Patch(Vector3D[] coords, Vector3D normal, double reflectance) {
 		this.coords = coords;
 		this.normal = normal;
 		this.reflectance = reflectance;
@@ -28,14 +28,13 @@ public class Patch {
 	 * @param coords
 	 * @return coords of the center point
 	 */
-	private double[] calcCenter(double[][] coords) {
-		double[] center = new double[3];
-		double[] pt1 = coords[0];
-		double[] pt2 = coords[1];
-		double[] pt3 = coords[2];
-		double[] pt4 = coords[3];
+	private Vector3D calcCenter(Vector3D[] coords) {
+		Vector3D pt1 = coords[0];
+		Vector3D pt2 = coords[1];
+		Vector3D pt3 = coords[2];
+		Vector3D pt4 = coords[3];
 		
-		center = calcMid(calcMid(pt1, pt2), calcMid(pt3, pt4));
+		Vector3D center = calcMid(calcMid(pt1, pt2), calcMid(pt3, pt4));
 
 		return center;
 	}
@@ -47,21 +46,19 @@ public class Patch {
 	 * @param pt2
 	 * @return coords of the midpoint
 	 */
-	private double[] calcMid(double[] pt1, double[] pt2) {
-		double[] midpoint = new double[3];
+	private Vector3D calcMid(Vector3D pt1, Vector3D pt2) {
+		double midpointX = (pt1.getX() + pt2.getX()) / 2;
+		double midpointY = (pt1.getY() + pt2.getY()) / 2;
+		double midpointZ = (pt1.getZ() + pt2.getZ()) / 2;
 
-		midpoint[0] = (pt1[0] + pt2[0]) / 2;
-		midpoint[1] = (pt1[1] + pt2[1]) / 2;
-		midpoint[2] = (pt1[2] + pt2[2]) / 2;
-
-		return midpoint;
+		return new Vector3D(midpointX, midpointY, midpointZ);
 	}
 
-	public double[][] getCoords() {
+	public Vector3D[] getCoords() {
 		return coords;
 	}
 
-	public double[] getNormal() {
+	public Vector3D getNormal() {
 		return normal;
 	}
 
@@ -69,7 +66,7 @@ public class Patch {
 		return reflectance;
 	}
 
-	public double[] getCenter() {
+	public Vector3D getCenter() {
 		return center;
 	}
 
